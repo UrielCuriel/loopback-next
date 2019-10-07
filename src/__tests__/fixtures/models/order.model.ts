e98e07ee2f28610f5986a7232cf13fe8cee83585
Uriel Curiel <urielcuriel@outlook.com>
2019-10-07 09:54:06 -0500

    last version @loopback/repository

---
+++ src/__tests__/fixtures/models/order.model.ts
@@ -0,0 +1,49 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: @loopback/repository
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {belongsTo, Entity, model, property} from '../../..';
+import {Customer, CustomerWithRelations} from './customer.model';
+import {
+  Shipment,
+  ShipmentWithRelations,
+} from '@loopback/repository-tests/dist/crud/relations/fixtures/models/shipment.model';
+import {Seller} from './seller.model';
+
+@model()
+export class Order extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'boolean',
+    required: false,
+  })
+  isShipped: boolean;
+
+  @belongsTo(() => Customer)
+  customerId: number;
+
+  @belongsTo(() => Seller)
+  sellerId: number;
+
+  @belongsTo(() => Shipment, {name: 'shipment'})
+  shipment_id: number;
+}
+
+export interface OrderRelations {
+  customer?: CustomerWithRelations;
+  shipment?: ShipmentWithRelations;
+}
+
+export type OrderWithRelations = Order & OrderRelations;
--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,6 +1,7 @@
-// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// Copyright IBM Corp. 2018. All Rights Reserved.
 // Node module: @loopback/repository
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-module.exports = require('./dist');
+export * from './entity-not-found.error';
+export * from './invalid-relation.error';
